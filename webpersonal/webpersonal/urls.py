867a41763a51886bf7dd1ad0562a8e5a810443b6
seerraa16 <114728385+seerraa16@users.noreply.github.com>
2024-11-15 19:04:54 +0100

    cambios 24

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from core import views
-
 urlpatterns = [
  path('', views.home, name="home"),
- path('about/', views.about, name="about"),
- path('portafolios/', views.portafolios, name="portfolios"),
- path('contacto/', views.contacto, name="contacto"),
+ path('about-me/', views.about, name="about"),
+ path('portfolio/', views.portafolios, name="portfolio"),
+ path('contact/', views.contacto, name="contact"),
  path('admin/', admin.site.urls),
 ]
+
