8e439be20215a1b0151e077b5505f4fc38316d37
seerraa16 <114728385+seerraa16@users.noreply.github.com>
2024-11-15 17:09:05 +0100

    cambios hechos

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,37 +1,9 @@
-from django.shortcuts import render
 from django.shortcuts import render, HttpResponse
-# Create your views here.
-html_base = """
- <h1>Mi Web Personal</h1>
- <ul>
- <li><a href="/">Portada</a></li>
- <li><a href="/about/">Acerca de</a></li>
- <li><a href="/portafolios/">Portafolios</a></li>
- <li><a href="/contacto/">contacto</a></li>
- </ul>
-"""
 def home(request):
  return render(request, "core/home.html")
-
 def about(request):
- return HttpResponse(html_base + """
- <h2>Acerca de</h2>
- <p>Me llamo Alejandro y me apasiona Django!</p>
- """)
-
-def portafolios(request):
- return HttpResponse(html_base + """
- <h2>Portafolios</h2>
- <p>Este es mi portafolio</p>
- """)
-
-def contacto(request):
- return HttpResponse (html_base + """
-    <h2>Contacto</h2>
-    <p>Les pongo a continuación mi gmail y mi Linkedin</p>
-    <ul>
-        <li><a href="mailto:a.serranocatalina@gmail.com">Gmail</a></li>
-        <li><a href="https://linkedin.com/in/alejandro-serrano-094145336">Linkedin</a></li>
-    </ul>              
-                     
- """)
+ return render(request, "core/about.html")
+def portfolio(request):
+ return render(request, "core/portfolio.html")
+def contact(request):
+ return render(request, "core/contact.html")
