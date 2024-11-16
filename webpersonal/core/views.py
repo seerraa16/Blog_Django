7eea83be7db07f0286c536b3c690478446e48acc
seerraa16 <114728385+seerraa16@users.noreply.github.com>
2024-11-16 12:56:26 +0100

    chill cambios

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+html_base = """
+    <h1>Mi Web Personal</h1>
+    <ul>
+        <li><a href="/">Portada</a></li>
+        <li><a href="/about/">Acerca de</a></li>
+        <li><a href="/contact/">Contacto</a></li>
+        <li><a href="/portfolio/">Portfolio</a></li>
+    </ul>
+        """
+
+
 def home(request):
- return render(request, "core/home.html")
+    return render(request, "core/home.html")
+
 def about(request):
- return render(request, "core/about.html")
-def portafolios(request):
- return render(request, "core/portafolios.html")
-def contacto(request):
- return render(request, "core/contacto.html")
+   return render(request, "core/about.html")
+
+def portfolio(request):
+    return render(request, "core/portfolio.html")
+   
+def contact(request):
+    return render(request, "core/contact.html")
--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from core import views
-urlpatterns = [
- path('', views.home, name="home"),
- path('about-me/', views.about, name="about"),
- path('portfolio/', views.portafolios, name="portfolio"),
- path('contact/', views.contacto, name="contact"),
- path('admin/', admin.site.urls),
-]
 
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('portfolio/', views.portfolio, name="portfolio"),
+    path('contact/', views.contact, name="contact"),
+    path('admin/', admin.site.urls),
+]
