1eb29f729d73f71240180ccc2366d41bd689193f
seerraa16 <114728385+seerraa16@users.noreply.github.com>
2024-11-18 19:54:32 +0100

    al fin funciona

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,26 +1,20 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-
 html_base = """
-    <h1>Mi Web Personal</h1>
-    <ul>
-        <li><a href="/">Portada</a></li>
-        <li><a href="/about/">Acerca de</a></li>
-        <li><a href="/contact/">Contacto</a></li>
-        <li><a href="/portfolio/">Portfolio</a></li>
-    </ul>
-        """
-
+<h1>Mi Web Personal</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about/">Acerca de</a></li>
+    <li><a href="/contact/">Contacto</a></li>
+</ul>
+"""
 
 def home(request):
     return render(request, "core/home.html")
-
 def about(request):
-   return render(request, "core/about.html")
-
-def portfolio(request):
-    return render(request, "core/portfolio.html")
-   
+    return render(request, "core/about.html")
 def contact(request):
     return render(request, "core/contact.html")
+def portfolio(request):
+    return render(request, "core/portfolio.html")
